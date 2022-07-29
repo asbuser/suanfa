343d42cb9371cd05b277f3f97b425d14a1d70f89
hht <964516890@qq.com>
2022-07-29 14:44:59 +0800

    third master

--- src/maopao/Test.java
+++ src/maopao/Test.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        Integer[] a = {9,6,5,8,4,2,3,6,1};
+        Integer[] a = {9, 6, 5, 8, 4, 2, 3, 6, 1};
         Maopao.sort(a);
         System.out.println(Arrays.toString(a));
         System.out.println("git修改");
         System.out.println("git测试合并");
         System.out.println("git测试合并冲突master");
         System.out.println("git测试合并冲突");
+        System.out.println("git测试push");
     }
 }
